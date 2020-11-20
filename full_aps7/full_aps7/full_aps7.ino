ffca982b6eee16f607e0269b0a289d1dabac19ec
sharpiestealr <62366339+sharpiestealr@users.noreply.github.com>
2020-11-20 09:07:06 -0300

    final update pls work pls work pls work

--- full_aps7/full_aps7/full_aps7.ino
+++ full_aps7/full_aps7/full_aps7.ino
@@ -108,7 +108,7 @@ static float calibra_TMP(float tensao)
 {
   float temperatura = 0.009787*tensao - 0.0025;//
   temperatura = temperatura*1000;
-  return temperatura;//
+  return temperatura;
 }
 
 void RGB_color(int sensor, int red, int green, int blue)
@@ -153,12 +153,12 @@ static int calcR(bool tempC, float temp, bool humidC, float humid)
     {
       if ((temp > check[0][1]) && (temp < check[0][0]))
       {
-        red = (temp-check[0][1])/((check[0][0]-check[0][1])*(color[0][0]-color[0][1]+color[0][1]));
+        red = (temp-check[0][1])/((check[0][0]-check[0][1])*(color[0][0]+color[0][1]));
       }
 
       else if ((temp > check[0][2]) && (temp < check[0][1]))
       {
-        red = (temp-check[0][2])/((check[0][1]-check[0][2])*(color[0][1]-color[0][2]+color[0][2]));
+        red = (temp-check[0][2])/((check[0][1]-check[0][2])*(color[0][1]+color[0][2]));
       }
     }
 
@@ -188,12 +188,12 @@ static int calcR(bool tempC, float temp, bool humidC, float humid)
     {
       if ((humid > check[1][1]) && (humid < check[1][0]))
       {
-        red = (humid-check[1][1])/((check[1][0]-check[1][1])*(color[0][0]-color[0][1]+color[0][1]));
+        red = (humid-check[1][1])/((check[1][0]-check[1][1])*(color[0][0]+color[0][1]));
       }
 
       else if ((humid > check[1][2]) && (humid < check[1][1]))
       {
-        red = (humid-check[1][2])/((check[1][1]-check[1][2])*(color[0][1]-color[0][2]+color[0][2]));
+        red = (humid-check[1][2])/((check[1][1]-check[1][2])*(color[0][1]+color[0][2]));
       }
     }
 
@@ -228,12 +228,12 @@ static int calcG(bool tempC, float temp, bool humidC, float humid)
     {
       if ((temp > check[0][1]) && (temp < check[0][0]))
       {
-        green = (temp-check[0][1])/((check[0][0]-check[0][1])*(color[1][0]-color[1][1]+color[1][1]));
+        green = (temp-check[0][1])/((check[0][0]-check[0][1])*(color[1][0]+color[1][1]));
       }
 
       else if ((temp > check[0][2]) && (temp < check[0][1]))
       {
-        green = (temp-check[0][2])/((check[0][1]-check[0][2])*(color[1][1]-color[1][2]+color[1][2]));
+        green = (temp-check[0][2])/((check[0][1]-check[0][2])*(color[1][1]+color[1][2]));
       }
     }
 
@@ -263,12 +263,12 @@ static int calcG(bool tempC, float temp, bool humidC, float humid)
     {
       if ((humid > check[1][1]) && (humid < check[1][0]))
       {
-        green = (humid-check[1][1])/((check[1][0]-check[1][1])*(color[1][0]-color[1][1]+color[1][1]));
+        green = (humid-check[1][1])/((check[1][0]-check[1][1])*(color[1][0]+color[1][1]));
       }
 
       else if ((humid > check[1][2]) && (humid < check[1][1]))
       {
-        green = (humid-check[1][2])/((check[1][1]-check[1][2])*(color[1][1]-color[1][2]+color[1][2]));
+        green = (humid-check[1][2])/((check[1][1]-check[1][2])*(color[1][1]+color[1][2]));
       }
     }
 
@@ -303,12 +303,12 @@ static int calcB(bool tempC, float temp, bool humidC, float humid)
     {
       if ((temp > check[0][1]) && (temp < check[0][0]))
       {
-        blue = (temp-check[0][1])/((check[0][0]-check[0][1])*(color[2][0]-color[2][1]+color[2][1]));
+        blue = (temp-check[0][1])/((check[0][0]-check[0][1])*(color[2][0]+color[2][1]));
       }
 
       else if ((temp > check[0][2]) && (temp < check[0][1]))
       {
-        blue = (temp-check[0][2])/((check[0][1]-check[0][2])*(color[2][1]-color[2][2]+color[2][2]));
+        blue = (temp-check[0][2])/((check[0][1]-check[0][2])*(color[2][1]+color[2][2]));
       }
     }
 
@@ -338,12 +338,12 @@ static int calcB(bool tempC, float temp, bool humidC, float humid)
     {
       if ((humid > check[1][1]) && (humid < check[1][0]))
       {
-        blue = (humid-check[1][1])/((check[1][0]-check[1][1])*(color[2][0]-color[2][1]+color[2][1]));
+        blue = (humid-check[1][1])/((check[1][0]-check[1][1])*(color[2][0]+color[2][1]));
       }
 
       else if ((humid > check[1][2]) && (humid < check[1][1]))
       {
-        blue = (humid-check[1][2])/((check[1][1]-check[1][2])*(color[2][1]-color[2][2]+color[2][2]));
+        blue = (humid-check[1][2])/((check[1][1]-check[1][2])*(color[2][1]+color[2][2]));
       }
     }
 
@@ -389,7 +389,8 @@ void loop()
   RGB_color(1, calcR(true, temp, false, umid), calcG(true, temp, false, umid), calcB(true, temp, false, umid));
   RGB_color(2, calcR(false, temp, true, umid), calcG(false, temp, true, umid), calcB(false, temp, true, umid));
   
-  lcd.clear(); 
+  lcd.clear();
+   
   lcd.setCursor(0, 0); 
   lcd.print("Resistance"); 
   lcd.print("[k");
@@ -398,12 +399,14 @@ void loop()
   lcd.setCursor(0,1);
   lcd.print(res);//prints resistance to lcd screen 
   delay(2000);
+  
   lcd.clear(); 
   lcd.setCursor(0, 0); 
   lcd.print("Light [Lux]:"); 
   lcd.setCursor(0,1);
   lcd.print(lum);//prints luminosity to lcd screen 
   delay(2000);
+  
   lcd.clear(); 
   lcd.setCursor(0, 0); 
   lcd.print("Temperature"); 
@@ -413,13 +416,21 @@ void loop()
   lcd.setCursor(0,1);
   lcd.print(temp);//prints temperature to lcd screen 
   delay(2000);
+  
   lcd.clear(); 
   lcd.setCursor(0, 0);
   lcd.print("Humidity [%]:"); 
   lcd.setCursor(0,1);
   lcd.print(umid);//prints humidity to lcd screen 
   delay(2000);//offsets the info update 
-  lcd.clear();
+
+  lcd.clear(); 
+  lcd.setCursor(0, 0);
+  lcd.print("Pressure [hPa]:"); 
+  lcd.setCursor(0,1);
+  lcd.print(pressure/100);//prints pressure to lcd screen 
+  delay(2000);//offsets the info update 
+
   
-  
+  lcd.clear(); 
 }
